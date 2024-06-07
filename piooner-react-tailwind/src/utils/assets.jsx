0ee15523b0c048896a3e960ec7278cff21e0814d
QuangBM138 <155440479+QuangBM138@users.noreply.github.com>
2024-06-07 10:57:00 +0700

    change link image

--- piooner-react-tailwind/src/utils/assets.jsx
+++ piooner-react-tailwind/src/utils/assets.jsx
@@ -16,8 +16,31 @@ export const Links = {
     "https://gombinhduong.com/gom-lai-thieu-net-dep-con-mai-voi-thoi-gian/",
 };
 
+const api = "https://cdn.builder.io/api/v1/image/assets";
 export const img = {
-  homeImg: "public/image/homepage_img.png",
+  // homeImg: "public/image/homepage_img.png",
+  // logoImg: "src/assets/image/logo.svg",
+  // cartIcon: "src/assets/image/icon_cart.svg",
+  // personIcon: "src/assets/image/icon_person.svg",
+  // foot01: "src/assets/image/logo_foot_1.svg",
+  // foot02: "src/assets/image/logo_foot_2.png",
+  // foot03: "src/assets/image/logo_foot_3.svg",
+  // foot04: "src/assets/image/logo_foot_4.svg",
+  // facebookIcon: "src/assets/image/icon_face.svg",
+  // arbutus01: "src/assets/image/aboutus.png",
+  // arbutus02: "src/assets/image/aboutus02.png",
+  // community: "src/assets/image/community.png",
+  // phoneIcon: "src/assets/image/icon_phone.svg",
+  // locationIcon: "src/assets/image/icon_locat.svg",
+  // emailIcon: "src/assets/image/icon_mail.svg",
+  // contactImg: "src/assets/image/contact_img.png",
+  // faceContact: "src/assets/image/icon_face_contact.svg",
+  // liney: "src/assets/image/liney.svg",
+  // backbutton: "src/assets/image/returnBut.svg",
+
+  homeImg:
+    api +
+    "/TEMP/3de14e10fbd69d714c2495317088eca9f2686d932d39f557d8beb8bcdc8cabf1?apiKey=101cc284a7074779856ab37fb68fa7a5&",
   logoImg: "src/assets/image/logo.svg",
   cartIcon: "src/assets/image/icon_cart.svg",
   personIcon: "src/assets/image/icon_person.svg",
@@ -27,7 +50,8 @@ export const img = {
   foot04: "src/assets/image/logo_foot_4.svg",
   facebookIcon: "src/assets/image/icon_face.svg",
   arbutus01:
-    "https://cdn.builder.io/api/v1/image/assets%2F101cc284a7074779856ab37fb68fa7a5%2F9d7953e72f4241deb1882e9ddba45c6f",
+    api +
+    "%2F101cc284a7074779856ab37fb68fa7a5%2F9d7953e72f4241deb1882e9ddba45c6f",
   arbutus02: "src/assets/image/aboutus02.png",
   community: "src/assets/image/community.png",
   phoneIcon: "src/assets/image/icon_phone.svg",
