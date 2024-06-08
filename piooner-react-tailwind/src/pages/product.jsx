f7495fa8347765f9cc3cd44fe2e35583d223c46b
ttdta <1000kgdat@gmail.com>
2024-06-08 11:09:18 +0700

    mot so anh + product moi

---
+++ piooner-react-tailwind/src/pages/product.jsx
@@ -0,0 +1,57 @@
+
+
+
+const product = () => {
+    return (
+      <div className="container">
+      <main className="main-content">
+        <div className="community-section">
+          <div className="images-list">
+            <div>
+              <img src="binh1.jpg" alt="Image 1" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh2.jpg" alt="Image 2" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh3.jpg" alt="Image 3" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh4.jpg" alt="Image 4" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh5.jpg" alt="Image 5" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh6.jpg" alt="Image 6" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh7.jpg" alt="Image 7" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh8.jpg" alt="Image 8" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh9.jpg" alt="Image 9" />
+              <h3>50.000vnd</h3>
+            </div>
+            <div>
+              <img src="binh10.jpg" alt="Image 10" />
+              <h3>50.000vnd</h3>
+            </div>
+          </div>
+        </div>
+      </main>
+      </div>
+    );
+  };
+
+export default product;
